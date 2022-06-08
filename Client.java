d40beb24d6b90c0be9bd1d4de42da769261654fc
Joao Pereira <misteringshow1@gmail.com>
2022-06-08 12:47:31 +0100

    Secure Sockets in action done

---
+++ Client.java
@@ -0,0 +1,85 @@
+
+
+import java.net.*;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.io.*;
+import java.util.Scanner;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+public class Client     {
+
+    private Socket socket = null;
+    private DataOutputStream out = null;
+    private DataInputStream in = null;
+    Scanner scanner  = null;
+    private String privateKeyPath = null;
+    private PrivateKey clientPrivateKey = null;
+
+    public Client(String address, int port, String clientKeyPath) throws IOException, InvalidKeyException, NoSuchPaddingException, 
+    NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException     {
+        try     {
+            socket = new Socket(address, port);
+            System.out.println("Connected\n");
+            System.out.println("Type: 'Exit' to close the connection\n");
+            // takes input from terminal
+            scanner = new Scanner(System.in);
+            // sends output to the socket
+            out = new DataOutputStream(socket.getOutputStream());
+            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            privateKeyPath = clientKeyPath;
+
+        }
+        catch(UnknownHostException u)   {
+            System.out.println(u);
+        }
+        catch(IOException i)    {
+            System.out.println(i);
+        }
+
+        //Getting Client private Key from the path
+        clientPrivateKey = (PrivateKey) Crypto.readKeyOrExit(privateKeyPath, "priv");
+        
+        //Reading the wrapped SecretKey and unwrapping it
+        int keySize = in.readInt();
+        byte []wrappedKey = in.readNBytes(keySize);
+
+        SecretKey secretKey = (SecretKey) Crypto.unWrapKey(clientPrivateKey, wrappedKey);
+
+        String line = "";
+        byte[] encryptedLine = null;
+
+        // keep reading until "Exit" is input
+        while (!line.equals("Exit"))    {
+            try     {
+                line = scanner.nextLine();
+                encryptedLine = Crypto.encrypt(line, secretKey);
+                out.writeInt(encryptedLine.length);
+                out.write(encryptedLine);
+            }
+            catch(IOException i)    {
+                System.out.println(i);
+                return;
+            }
+        }
+        // close the connection
+        try     {
+            out.close();
+            in.close();
+            socket.close();
+        }
+        catch(IOException i)    {
+            System.out.println(i);
+        }
+    }
+    public static void main(String args[]) throws InvalidKeyException, IOException, NoSuchPaddingException, 
+    NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+        new Client("127.0.0.1", 5000, "private_key.der");
+    }
+}
---
+++ Server.java
@@ -0,0 +1,79 @@
+
+
+
+import java.net.*;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+import java.io.*;
+
+public class Server     {
+
+    private Socket socket = null;
+    private ServerSocket server = null;
+    private DataInputStream in = null;
+    private DataOutputStream out = null;
+    private String publicKeyPath = null;
+    private PublicKey clientPublicKey = null;
+
+    public Server(int port, String clientKeyPath) throws NoSuchAlgorithmException, InvalidKeyException, 
+    IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, InvalidAlgorithmParameterException     {
+        try     {
+            server = new ServerSocket(port);
+            System.out.println("Server started");
+            System.out.println("Waiting for a client ...");
+            socket = server.accept();
+            System.out.println("Client accepted");
+            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            out = new DataOutputStream(socket.getOutputStream());
+            publicKeyPath = clientKeyPath;
+            String line = "";
+
+            //Getting pubblic Key of the client
+            clientPublicKey = (PublicKey) Crypto.readKeyOrExit(publicKeyPath, "pub");
+
+            //Generating the secret Key for connection
+            SecretKey secretKey = Crypto.generate(256);
+
+            //secretKey wrapped with client pub key
+            byte[] wrappedKey = Crypto.wrapKey(clientPublicKey, secretKey);
+
+            //Sending the wrapped Secret Key
+            out.writeInt(wrappedKey.length);
+            out.write(wrappedKey);
+
+            // reads message from client until "Exit" is sent
+            while (!line.equals("Exit"))    { //Not sure Se continua assim
+                try     {
+                    int encryptedSize = in.readInt();
+                    byte[] encryptedKey = in.readNBytes(encryptedSize);
+                    line = Crypto.decrypt(encryptedKey, secretKey);
+                    System.out.println(line);
+                }
+                catch(IOException i)    {
+                    System.out.println(i);
+                    return;
+                }
+            }
+            System.out.println("Closing connection");
+            // close connection
+            socket.close();
+            in.close();
+            out.close();
+        }
+        catch(IOException i)    {
+            System.out.println(i);
+        }
+    }
+    public static void main(String args[]) throws InvalidKeyException, NoSuchAlgorithmException, 
+    IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, InvalidAlgorithmParameterException{
+        new Server(5000, "public_key.der");
+    }
+}
